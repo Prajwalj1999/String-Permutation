530a43d185081574246df7af3cafeb9b1b702fc9
Prajwal J Shetty <46040105+Prajwalj1999@users.noreply.github.com>
2020-07-26 16:46:24 +0530

    Add files via upload

---
+++ StringPermutation.py
@@ -0,0 +1,25 @@
+str=input("Enter a String:")
+strList=list(str)
+NewStr=list(str)
+val={}
+for item in strList:
+    if val.get(item,0)==0:
+        val[item]=1
+    else:
+        val[item]=val[item]+1
+
+StrLen=len(NewStr)
+def permute(valDict,level):
+    if level==0:
+        print(''.join(NewStr) )
+    else:
+        for item in valDict.keys():
+            if valDict[item]>0:
+                NewVal=valDict.copy()
+                NewStr[StrLen-level]=item
+                NewVal[item]=NewVal[item]-1
+                permute(NewVal,level-1)
+
+
+
+permute(val,len(NewStr) )
